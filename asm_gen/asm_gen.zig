177e9e97f3b62f1d3b4c3211c84ec4ec7e7ca557
Shikiyo <shikiyo@archjk>
2022-08-11 00:19:20 -0300

    Removing commented debug panic

--- asm_gen/asm_gen.zig
+++ asm_gen/asm_gen.zig
@@ -250,7 +250,6 @@ const MetaStruct = struct {
                         }
                         break;
                     }
-                    // @panic("asdfadf");
                 }
             }
         }
