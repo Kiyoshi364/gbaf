b24e866882c611b5bfc1430bf3408fc08f68b978
Shikiyo <shikiyo@archjk>
2022-08-16 11:46:04 -0300

    Moving some tests to header

--- asm_gen/header.zig
+++ asm_gen/header.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const assert = std.debug.assert;
+const testing = std.testing;
 
 fn OfftableEntry(comptime Bsize: type) type {
     assert( Bsize == u16 or Bsize == u32 );
@@ -15,4 +16,53 @@ fn OfftableEntry(comptime Bsize: type) type {
             fixed: Bsize,
         };
     };
+}
+
+test "OfftableEntry compiles" {
+    const OE16 = OfftableEntry(u16);
+    testing.refAllDecls(OE16);
+    const OE32 = OfftableEntry(u32);
+    testing.refAllDecls(OE32);
+}
+
+fn hasPrefix(name: []const u8, prefix: []const u8) bool {
+    return for (prefix) |c, i| {
+        if ( name[i] != c ) break false;
+    } else true;
+}
+
+test "Thumb's OfftableEntry fixed has value, non-fixed don't" {
+    const thumbFields = @typeInfo(Thumb).Union.fields;
+    inline for (thumbFields) |ufield| {
+        const UField = ufield.field_type;
+        assert( @hasDecl(UField, "offtable") );
+        for ( @field(UField, "offtable") ) |entry| {
+            const Enum = @TypeOf(entry).Enum;
+            if ( hasPrefix(entry.name, "fixed") ) {
+                try testing.expectEqual(
+                    Enum.fixed, @as(Enum, entry.isFixed));
+            } else {
+                try testing.expectEqual(
+                    Enum.nil, @as(Enum, entry.isFixed));
+            }
+        }
+    }
+}
+
+test "Arm's OfftableEntry fixed has value, non-fixed don't" {
+    const armFields = @typeInfo(Arm).Union.fields;
+    inline for (armFields) |ufield| {
+        const UField = ufield.field_type;
+        assert( @hasDecl(UField, "offtable") );
+        for ( @field(UField, "offtable") ) |entry| {
+            const Enum = @TypeOf(entry).Enum;
+            if ( hasPrefix(entry.name, "fixed") ) {
+                try testing.expectEqual(
+                    Enum.fixed, @as(Enum, entry.isFixed));
+            } else {
+                try testing.expectEqual(
+                    Enum.nil, @as(Enum, entry.isFixed));
+            }
+        }
+    }
 }
